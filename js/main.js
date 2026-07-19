809e718ac8d300c6ee3e8058d861a3002638584c
lilitantonyan <lilit@helix.am>
2026-07-19 19:17:32 +0400

    add changes

--- js/main.js
+++ js/main.js
@@ -20,21 +20,48 @@ document.addEventListener('DOMContentLoaded', () => {
     let tabItems = document.querySelectorAll('.tab__link');
     let btnOrange = document.querySelectorAll('.store__item');
     let modal = document.querySelector('.modal')
-    let modalClose = document.querySelector('.btn__close')
-    tabItems.forEach(tabItem => {
-        tabItem.addEventListener('click', function (evt) {
-            const alias = this.dataset.alias;
-            opentab(evt, alias);
+    let modalClose = document.querySelector('.btn__close');
+    if(tabItems){
+        tabItems.forEach(tabItem => {
+            tabItem.addEventListener('click', function (evt) {
+                const alias = this.dataset.alias;
+                opentab(evt, alias);
+            });
         });
-    });
-    btnOrange.forEach(btn => {
-        btn.addEventListener('click', function (evt) {
+    }
+
+    if(btnOrange){
+        btnOrange.forEach(btn => {
+            btn.addEventListener('click', function (evt) {
+                evt.preventDefault();
+                modal.classList.add('open');
+            });
+        });
+    }
+
+    if(modalClose){
+        modalClose.addEventListener('click', function (evt) {
             evt.preventDefault();
-            modal.classList.add('open');
+            modal.classList.remove('open');
         });
-    });
-    modalClose.addEventListener('click', function (evt) {
-        evt.preventDefault();
-        modal.classList.remove('open');
-    });
+    }
+
+
+    let btnGo =  document.querySelectorAll('.task__action');
+    if(btnGo){
+        btnGo.forEach(btn => {
+            btn.addEventListener('click', function (evt) {
+                if (!this.querySelector('.btn__go').classList.contains('hide')) {
+                    this.querySelector('.btn__go').classList.add('hide');
+                    this.querySelector('.btn__load').classList.add('show');
+                    setTimeout(function () {
+                        btn.querySelector('.btn__load').classList.remove('show');
+                        btn.querySelector('.btn__done').classList.add('show');
+                    }, 3000);
+                }
+            });
+        });
+    }
+
+
 });
